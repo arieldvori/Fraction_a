f0bef53ea377fcf20c4263dd457c2b06c48cab1e
Ariel Dvori <arieldv102@gmail.com>
2023-04-18 17:40:34 +0300

    first

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -2,12 +2,30 @@
 #include <iostream>
 using namespace std;
 
-Fraction::Fraction(int t, int b){
+ariel::Fraction::Fraction(int t, int b){
     _t=t;
     _b=b;
 }
 
-//overload the operator <<. 
-std::ostream& operator<<(std::ostream &s, const Fraction &fraction){
-    return s << fraction._t << std::endl << "___" << std::endl << fraction._b << std::endl;
+ariel::Fraction::Fraction(float t, float b){
+    float_t=t;
+    float_b=b;
 }
+
+int ariel::Fraction::gcd(int a, int b){
+    while (b!=0){
+        int temp = b;
+        b = a % b;
+        a=temp;
+    }
+    return a;
+}
+void ariel::Fraction::reduse(){
+    int num = this->gcd(this->_t,this->_b);
+    this->_t=this->_t/num;
+    this->_b=this->_b/num;
+}
+
+
+
+
